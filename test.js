6ebdafbd9901333cef5ccc1dfe9aa4285b5a3b6b
Ridho Prasetya <ndiing.inc@gmail.com>
2022-08-12 15:48:22 +0700

    save

--- test.js
+++ test.js
@@ -1,36 +1,41 @@
 const TransparentProxy = require('./index')
 
 const proxy = new TransparentProxy()
-// proxy.on(/(request|response)$/, console.log)
-
-// callback
-// call twice
-// on beforeRequest and on beforeResponse
-proxy.use('https://jsonplaceholder.typicode.com/posts', (req,res,next) => {
-    // when not null
-    // indicate beforeRequest
-    if(req){
-        req.headers.delete('if-none-match')
-    }
-    // when not null
-    // indicate beforeResponse
-    if(res){
-        res.headers.set('x-powered-by','ndiing')
-        // res.body=JSON.parse(res.body)
-        // res.body=JSON.stringify(res.body)
-        res.body=JSON.stringify([
-            {
-                title: 'Intercept response',
-                message: 'Example intercept response'
-            }
-        ])
-    }
-    next()
+proxy.on(/(request|response)$/, (eventName,doc) => {
+    // doc=request/response
+    console.log(eventName)
 })
 
-proxy.listen(8888, () => {
-    console.log('proxy listen on port 8888')
-})
+// // callback
+// // call twice
+// // on beforeRequest and on beforeResponse
+// proxy.use('https://jsonplaceholder.typicode.com/posts', (req,res,next) => {
+//     // when not null
+//     // indicate beforeRequest
+//     if(req){
+//         req.headers.delete('if-none-match')
+//     }
+//     // when not null
+//     // indicate beforeResponse
+//     if(res){
+//         res.headers.set('x-powered-by','ndiing')
+//         res.body=JSON.parse(res.body)
+//         res.body=JSON.stringify(res.body)
+//     }
+//     next()
+// })
+
+// proxy.use('https://www.youtube.com/.*', (req,res,next) => {
+//     if(req){}
+//     if(res){
+//         res.body='Hello YouTube'
+//     }
+//     next()
+// })
+
+// proxy.listen(8888, () => {
+//     console.log('proxy listen on port 8888')
+// })
 
 // setTimeout(() => {
 //     // stop proxy
