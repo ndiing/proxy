4dbe44b84651c6d3d1b513a2402c953a5606c35d
Ridho Prasetya <ndiing.inc@gmail.com>
2022-08-12 11:21:52 +0700

    update external

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-require('./index')
+const TransparentProxy = require('./index')
+
+const proxy = new TransparentProxy()
+
+proxy.listen(8888, () => {
+    console.log('proxy listen on port 8888')
+})
+
+// setTimeout(() => {
+//     // stop proxy
+//     proxy.close()
+//     console.log('proxy stopped')
+// }, 1000+1)
