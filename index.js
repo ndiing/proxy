80355dd84403cf3f4e6c16e9ac5652e78bd09f7e
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-14 12:24:18 +0700

    commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-require('./src/lib/proxy.js')
+const {
+    setProxyServer,
+    getProxyServer,
+    start,
+    stop,
+}=require('./src/lib/proxy.js')
 // require('./src/index.js')
+
+module.exports={
+    setProxyServer,
+    getProxyServer,
+    start,
+    stop,
+}
